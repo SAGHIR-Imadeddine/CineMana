019cedc7f82472b72614fb132a4f7f35b7b07dd1
SAGHIR-Imadeddine <sag.imadeddine@gmail.com>
2024-10-15 15:29:48 +0100

    Feat: connecting the DB and starting the server

---
+++ BackEnd/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { conDB } from './Config/db.js';
+// import logger from './Config/logger.js';
+
+dotenv.config({ path: 'backend/.env'});
+const port = process.env.PORT || 3000;
+
+
+const app = express();
+conDB();
+
+
+app.use(express.json());
+
+
+
+// app.use((err, req, res, next) => {
+//     logger.error(err.message);
+//     res.status(500).send('Something broke!');
+// });
+  
+app.listen(port, () => console.log(`Server is running on port : ${port}`));
